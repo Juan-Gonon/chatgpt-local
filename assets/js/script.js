abcb96a7e95423283499f6c120344fda71b21f0d
Juan Gonon <juanfegonon27@gmail.com>
2024-06-19 22:36:26 -0600

    agregar mensaje y scroll

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const $ = el =>  document.querySelector(el)
+
+const $form = $('form')
+const $input = $('input')
+const $template = $('#message-template')
+const $messages = $('ul')
+const $container = $('main')
+const $button = $('button')
+
+
+$form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const messageText = $input.value.trim()
+
+    if(messageText !== ''){
+        $input.value = ''
+    }
+
+    addMessage(messageText, 'user')
+
+})
+
+function addMessage(text, sender){
+    const clonedTemplate = $template.content.cloneNode(true)
+    const $newMessage = clonedTemplate.querySelector('.message')
+    const $who = $newMessage.querySelector('span')
+    const $text = $newMessage.querySelector('p')
+
+    $text.textContent = text
+    $who.textContent = sender === 'bot' ? 'GPT' : 'Tú'
+
+    $newMessage.classList.add(sender)
+
+    
+    // actualizar el scroll
+    
+    $messages.appendChild($newMessage)
+    
+    $container.scrollTop = $container.scrollHeight
+
+}
